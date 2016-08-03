ba5bee8dafe2734a8035fb2174a886f47197df30
Google1234 <taoJiang_ict@163.com>
2016-08-03 22:14:58 +0800

    bug still

--- 4.py
+++ 4.py
@@ -5,27 +5,39 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
-        m=len(nums1)
-        n=len(nums2)
-        if m<n:
-            return self.findMedianSortedArrays(nums2,nums1)
-        low_l=low_r=0
-        high_l=high_r=m-1
-        while low_l<=high_r:
-            mid_l=(low_l+low_r)/2
-            mid_r=(high_l+high_r+1)/2
-            cut1_l=nums1[mid_l]
-            cut1_r=nums1[mid_r]
-            cut2_l=nums2[(m+n-mid_l-mid_r-2)/2]
-            cut2_r=nums2[(m+n-mid_l-mid_r-1)/2]
-            if cut1_l>cut2_r:           #shoule move to left
-                last_l=high_l=mid_l
-                last_r=high_r=mid_r
-            elif cut2_l>cut1_r:         #shoule moce t0 right
-                low_l=mid_l
-                low_r=mid_r
+        len1=len(nums1)
+        len2=len(nums2)
+        if len1<len2:
+            self.findMedianSortedArrays(nums2,nums1)
+        half=(len1+len2)/2
+        i_left=1
+        i_right=len1
+        while i_left<=i_right:
+            i=(i_left+i_right)/2
+            j=half-i #numbers of (nums1 and nums2 left part)<= numbers of (nums1 and nums2 right part)
+            if j<0 or (i>0 and  j<len2 and nums1[i-1]>nums2[j]):
+                i_right=i-1
+            elif j>len2+1 or (i<len1 and j>1 and nums1[i]<nums2[j-1]):
+                i_left=i+1
             else:
-                return (max(cut1_l,cut2_l)+min(cut1_r,cut2_r))*1.0/2
+                if i_right==1:
+                    return (nums1[i_right-1]+nums2[len2-1])*1.0/2
+                if i_left==len1:
+                    return (nums1[i_left-1]+nums2[0])*1.0/2
+
+                if j==0:
+                    min_value_nums2_right = nums2[j]
+                    max_value_nums2_left=nums1[i-1]
+                elif j==len2:
+                    min_value_nums2_right=nums1[i]
+                    max_value_nums2_left = nums2[j - 1]
+                else:
+                    min_value_nums2_right=nums2[j]
+                    max_value_nums2_left = nums2[j - 1]
+                if (len1+len2)%2==0:
+                    return (max(nums1[i-1],max_value_nums2_left)+min(nums1[i]+min_value_nums2_right))*1.0/2
+                else:
+                    return min(nums1[i],min_value_nums2_right)
 
 a=Solution()
-print a.findMedianSortedArrays([1],[3,4])
+print a.findMedianSortedArrays([],[2,3])
