68768aa2da9a457361aacd7981e9a39b201826f1
Google1234 <taoJiang_ict@163.com>
2016-08-06 10:50:22 +0800

    6 solved

--- number.py
+++ number.py
@@ -1,42 +1,27 @@
 class Solution(object):
-    def longestPalindrome(self, s):
+    def convert(self, s, numRows):
         """
         :type s: str
+        :type numRows: int
         :rtype: str
         """
-
-        if len(s)==0:
-            return ''
-
-        s_new='^'
-        for a in s:
-            s_new+='#'+a
-        s_new+='#$'
-        count=[0 for i in range(len(s_new))]
-
-        C=R=0
-        for i in range(len(s_new)-1):
-            i_mirror = C - (i - C)
-            if i>R:
-                count[i]=0
-            else:
-                count[i] = min(count[i_mirror],R - i)
-
-            while s_new[i + count[i] + 1] == s_new[i - count[i] - 1]:
-                count[i] += 1
-            if i+count[i]>R:
-                C = i
-                R = i + count[i]
-        max_count=0
-        pointer=0
-        for i in range(len(count)):
-            if count[i]>max_count:
-                max_count=count[i]
-                pointer=i
-        longest_substring=''
-        for i in s_new[pointer-max_count:pointer+max_count+1]:
-            if i!='#':
-                longest_substring+=i
-        return longest_substring
-
-
+        s_new=''
+        if numRows==1:
+            return s
+        for i in range(numRows):
+            pointer=i
+            if pointer<len(s):
+                s_new+=s[pointer]
+            odd=0
+            while True:
+                if odd%2==0:
+                    increase=2*(numRows-1-i)
+                else:
+                    increase=2*i
+                if increase!=0:
+                    pointer += increase
+                    if pointer>=len(s):
+                        break
+                    s_new += s[pointer]
+                odd+=1
+        return s_new
