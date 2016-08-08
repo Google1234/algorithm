ff7f7aee2dc3c36b2ecbf7f387c958b76002178c
Google1234 <taoJiang_ict@163.com>
2016-08-08 20:38:38 +0800

    9 solved

--- number.py
+++ number.py
@@ -1,39 +1,23 @@
 class Solution(object):
-    def myAtoi(self, str):
+    def isPalindrome(self, x):
         """
-        :type str: str
-        :rtype: int
+        :type x: int
+        :rtype: bool
         """
-        pointer=0
-        while pointer<len(str):
-            t=str[pointer]
-            if t==' ':
-                pointer+=1
-            elif t=='+' or t=='-' or (ord(t)-48>=0 and ord(t)-48<=9):
-                value = 0
-                if t=='+':
-                    posotive=1
-                elif t=='-':
-                    posotive=-1
-                else:
-                    posotive=1
-                    value=ord(t)-48
-                pointer += 1
-                if pointer==len(str) :
-                    return value
-                while pointer<len(str):
-                    t=ord(str[pointer])-48
-                    if t>=0 and t<=9:
-                        value=value*10+t
-                        pointer+=1
-                    else:
-                        break
-                if posotive*value>2147483647:
-                    return 2147483647
-                elif posotive*value<-2147483648:
-                    return -2147483648
-                else:
-                    return posotive*value
+        if x<0:
+            return False
+        t=0
+        while x/(10**t)!=0:
+            t+=1
+        t_max=t
+        t=1
+        while t_max>=t:
+            if ((x%(10**t))/(10**(t-1)))==((x/(10**(t_max-1))%10)):
+                t+=1
+                t_max-=1
             else:
-                return 0
-        return 0
+                break
+        if t_max<t:
+            return True
+        else:
+            return False
--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-print 65536*65536
+import sys
+
+print sys.maxint
+print -2**31-1
+print -2**63 - 1
+print 2**63
