a05d2f9485c603749522900d1a3318f9ff3ece9d
Google1234 <taoJiang_ict@163.com>
2016-08-10 23:54:53 +0800

    10 not solved

--- number.py
+++ number.py
@@ -1,23 +1,19 @@
 class Solution(object):
-    def isPalindrome(self, x):
+    def isMatch(self, s, p):
         """
-        :type x: int
+        :type s: str
+        :type p: str
         :rtype: bool
         """
-        if x<0:
-            return False
-        t=0
-        while x/(10**t)!=0:
-            t+=1
-        t_max=t
-        t=1
-        while t_max>=t:
-            if ((x%(10**t))/(10**(t-1)))==((x/(10**(t_max-1))%10)):
-                t+=1
-                t_max-=1
-            else:
-                break
-        if t_max<t:
-            return True
-        else:
-            return False
+        dp=[[False for i in range(len(s)+1)] for j in range(len(p)+1)]
+        dp[0][0]=True
+        for i in range(1,len(p)+1):
+            for j in range(len(s)+1):
+                if p[i-1]=='*':
+                    dp[i][j]=dp[i-2][j] or ((p[i-2]=='.' or p[i-2]==s[j-1])and dp[i-1][j])
+                else:
+                    dp[i][j]=j>0 and (p[i-1]==s[j-1] or p[i-1]=='.') and dp[i-1][j-1]
+        return dp[len(p)][len(s)]
+
+a=Solution()
+print a.isMatch("aa","a*")
