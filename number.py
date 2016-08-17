24e1581995a70c0773200c65a272f4839c0fa5c3
Google1234 <taoJiang_ict@163.com>
2016-08-17 22:37:28 +0800

    292 solved

--- number.py
+++ number.py
@@ -1,15 +1,10 @@
 class Solution(object):
-    def reverseString(self, s):
+    def canWinNim(self, n):
         """
-        :type s: str
-        :rtype: str
+        :type n: int
+        :rtype: bool
         """
-        if len(s)==0:
-            return ''
-        s=list(s)
-        l=len(s)
-        for i in range((l+1)/2):
-            t=s[i]
-            s[i]=s[l-1-i]
-            s[l-1-i]=t
-        return ''.join(s)
+        if n%4!=0:
+            return True
+        else:
+            return False
