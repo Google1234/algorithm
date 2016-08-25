1c9f2c1f29a142be1d17ca3d97c8f60469cd0c56
Google1234 <taoJiang_ict@163.com>
2016-08-25 21:43:48 +0800

    21 solved

--- number.py
+++ number.py
@@ -1,21 +1,25 @@
 class Solution(object):
-    def removeNthFromEnd(self, head, n):
+    def isValid(self, s):
         """
-        :type head: ListNode
-        :type n: int
-        :rtype: ListNode
+        :type s: str
+        :rtype: bool
         """
-        pointer=head
-        for i in xrange(n):
-            if pointer!=None:
-                pointer=pointer.next
+        n=len(s)
+        if n==0:
+            return True
+        if n%2!=0:
+            return False
+        stack=[' ' for i in xrange(n+1)]
+        length=0
+        for i in xrange(len(s)):
+            if s[i]=="(" or s[i]=="{" or s[i]=="[":
+                length += 1
+                stack[length]=s[i]
+            elif (s[i]==")" and stack[length]=="(") or (s[i]=="]" and stack[length]=="[") or (s[i]=="}" and stack[length]=="{"):
+                length-=1
             else:
-                return None
-        if pointer==None:
-            return head.next
-        before=head
-        while pointer.next!=None:
-            pointer=pointer.next
-            before=before.next
-        before.next=before.next.next
-        return head
+                return False
+        if length==0:
+            return True
+        else:
+            return False
