98fa06f92f283bd33a67db85b5d2d1e4441b2b1f
Google1234 <taoJiang_ict@163.com>
2016-08-27 20:14:18 +0800

    22 solved

--- number.py
+++ number.py
@@ -1,32 +1,18 @@
 class Solution(object):
-    def mergeTwoLists(self, l1, l2):
+    def generateParenthesis(self, n):
         """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
+        :type n: int
+        :rtype: List[str]
         """
-        if l1==None :
-            return l2
-        if l2==None:
-            return l1
-        pointer1=l1
-        pointer2=l2
-        begin=last=ListNode(0)
-        while pointer1!=None and pointer2!=None:
-            if pointer1.val>pointer2.val:
-                last.next=pointer2
-                pointer2=pointer2.next
-            else:
-                last.next=pointer1
-                pointer1=pointer1.next
-            last=last.next
-        while pointer1!=None:
-            last.next=pointer1
-            last=last.next
-            pointer1=pointer1.next
-        while pointer2 != None:
-            last.next = pointer2
-            last=last.next
-            pointer2 = pointer2.next
-        return begin.next
-
+        if n==0:
+            return []
+        self.resilt=[]
+        self.generate(n,n,"")
+        return self.resilt
+    def generate(self,left_remain,right_remain,current):
+        if right_remain==0:
+            self.resilt.append(current)
+        if left_remain>0:
+             self.generate(left_remain-1,right_remain,current+"(")
+        if right_remain>left_remain:
+             self.generate(left_remain,right_remain-1,current+")")
