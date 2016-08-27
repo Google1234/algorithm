eab339d1f4645cbea440b81be7ff4fdfe420a823
Google1234 <taoJiang_ict@163.com>
2016-08-27 20:50:22 +0800

    23 solved

--- number.py
+++ number.py
@@ -1,18 +1,35 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+         self.val = x
+         self.next = None
+
 class Solution(object):
-    def generateParenthesis(self, n):
+    def mergeKLists(self, lists):
         """
-        :type n: int
-        :rtype: List[str]
+        :type lists: List[ListNode]
+        :rtype: ListNode
         """
-        if n==0:
+        if len(lists)==0:
             return []
-        self.resilt=[]
-        self.generate(n,n,"")
-        return self.resilt
-    def generate(self,left_remain,right_remain,current):
-        if right_remain==0:
-            self.resilt.append(current)
-        if left_remain>0:
-             self.generate(left_remain-1,right_remain,current+"(")
-        if right_remain>left_remain:
-             self.generate(left_remain,right_remain-1,current+")")
+        if len(lists)==1:
+            return lists[0]
+        if len(lists)==2:
+           Node1=lists[0]
+           Node2=lists[1]
+           begin=Last=ListNode(0)
+           while Node1!=None and Node2!=None:
+               if Node1.val>Node2.val:
+                   Last.next=Node2
+                   Node2=Node2.next
+               else:
+                   Last.next=Node1
+                   Node1=Node1.next
+               Last=Last.next
+           if Node1!=None:
+               Last.next=Node1
+           if Node2!=None:
+               Last.next=Node2
+           return begin.next
+        else:
+            return self.mergeKLists([self.mergeKLists(lists[:len(lists)/2]),self.mergeKLists(lists[len(lists)/2:])])
