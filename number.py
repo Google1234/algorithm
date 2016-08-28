9d9e2887e49981166126b7a75dd4f751cea4f604
Google1234 <taoJiang_ict@163.com>
2016-08-28 19:16:41 +0800

    24 solved

--- number.py
+++ number.py
@@ -1,35 +1,26 @@
-# Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, x):
-         self.val = x
-         self.next = None
-
 class Solution(object):
-    def mergeKLists(self, lists):
+    def swapPairs(self, head):
         """
-        :type lists: List[ListNode]
+        :type head: ListNode
         :rtype: ListNode
         """
-        if len(lists)==0:
-            return []
-        if len(lists)==1:
-            return lists[0]
-        if len(lists)==2:
-           Node1=lists[0]
-           Node2=lists[1]
-           begin=Last=ListNode(0)
-           while Node1!=None and Node2!=None:
-               if Node1.val>Node2.val:
-                   Last.next=Node2
-                   Node2=Node2.next
-               else:
-                   Last.next=Node1
-                   Node1=Node1.next
-               Last=Last.next
-           if Node1!=None:
-               Last.next=Node1
-           if Node2!=None:
-               Last.next=Node2
-           return begin.next
-        else:
-            return self.mergeKLists([self.mergeKLists(lists[:len(lists)/2]),self.mergeKLists(lists[len(lists)/2:])])
+        if head==None or head.next==None:
+            return head
+        left=head
+        right=head.next
+        begin=last=ListNode(0)
+        while True:
+            last.next=right
+            left.next=right.next
+            right.next=left
+            last=left
+            left=left.next
+            if left==None:
+                last.next=left
+                break
+            right=left.next
+            if right==None:
+                last.next=left
+                break
+        return begin.next
+
