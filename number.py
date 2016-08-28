831698f94b969437733bea02c3a7789132e623d2
Google1234 <taoJiang_ict@163.com>
2016-08-28 21:52:41 +0800

    25 solved

--- number.py
+++ number.py
@@ -1,26 +1,44 @@
+# Definition for singly-linked list.
+class ListNode(object):
+     def __init__(self, x):
+         self.val = x
+         self.next = None
+
 class Solution(object):
-    def swapPairs(self, head):
+    def reverseKGroup(self, head, k):
         """
         :type head: ListNode
+        :type k: int
         :rtype: ListNode
         """
-        if head==None or head.next==None:
+        if head==None or k==1:
             return head
-        left=head
-        right=head.next
-        begin=last=ListNode(0)
-        while True:
-            last.next=right
-            left.next=right.next
-            right.next=left
-            last=left
-            left=left.next
-            if left==None:
-                last.next=left
-                break
-            right=left.next
-            if right==None:
-                last.next=left
-                break
-        return begin.next
 
+        last2=current=head
+        count=1
+        begin=last1=before=ListNode(0)
+        while current:
+            after=current.next
+            current.next=before
+            before=current
+            current=after
+            if count%k==0:
+                last1.next=before
+                last1=last2
+                last2=current
+            count+=1
+        if count%k!=1:
+            current=before
+            after=None
+            count-=1
+            while count%k!=1:
+                before=current.next
+                current.next=after
+                after=current
+                current=before
+                count-=1
+            current.next=after
+            last1.next=current
+        else:
+            last1.next=None
+        return begin.next
