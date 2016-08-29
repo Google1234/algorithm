bc4f854e7e6d3a1abe989fb8139749ac0bae77ae
Google1234 <taoJiang_ict@163.com>
2016-08-29 23:18:29 +0800

    27 solved

--- number.py
+++ number.py
@@ -1,22 +1,27 @@
 class Solution(object):
-    def removeDuplicates(self, nums):
+    def removeElement(self, nums, val):
         """
         :type nums: List[int]
+        :type val: int
         :rtype: int
         """
         length=len(nums)
-        if length==0:
-            return 0
-        begin=stop=0
-        pointer=0
+        if length==0 or val==None:
+            return length
+        left=0
+        right=length-1
         while True:
-            while stop<length and nums[begin]==nums[stop]:
-                stop+=1
-            if stop==length:
-                break
-            nums[pointer]=nums[begin]
-            pointer+=1
-            begin=stop
-        nums[pointer]=nums[begin]
-        return pointer+1
-
+            while nums[left]!=val:
+                left+=1
+                if left>=right:
+                    if nums[right]==val:
+                        return right
+                    else:
+                        return right+1
+            while nums[right]==val:
+                right-=1
+                if right<=left:
+                    return left
+            nums[left]=nums[right]
+            left+=1
+            right-=1
