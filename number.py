bf675bec85d1fcf08a0b863de2985c8d26e7c7a7
Google1234 <taoJiang_ict@163.com>
2016-09-02 23:41:34 +0800

    ..

--- number.py
+++ number.py
@@ -12,29 +12,45 @@ class Solution(object):
         word_len=len(words[0])
         if s_len<words_nums*word_len :
             return []
+        result = []
         dict={}
         for word in words:
             if dict.has_key(word):
                 dict[word]+=1
             else:
                 dict[word]=1
-        result=[]
-        for i in xrange(s_len-word_len+1):
-            dict1 = {}
+        dict1={}
+        for key in dict.keys():
+            dict1[key]=0
+
+        for begin in xrange(word_len):
+            pointer=begin
+            for key in dict.keys():
+                dict1[key] = 0
             n=0
-            while True:
-                word = s[i+n*word_len:i + (n+1)*word_len]
+            while pointer+(n+1)*word_len<=s_len:
+                word=s[pointer+n*word_len:pointer+(n+1)*word_len]
                 if dict.has_key(word):
-                    if dict1.has_key(word):
-                        dict1[word]+=1
-                    else:
-                        dict1[word]=1
+                    dict1[word]+=1
                     if dict1[word]>dict[word]:
-                        break
+                        for i in range(n):
+                            word1=s[pointer+i*word_len:pointer+(i+1)*word_len]
+                            dict1[word1]-=1
+                            if word1==word:
+                                break
+                        pointer=pointer+(i+1)*word_len
+                        n-=i
+                    else:
+                        n+=1
+                        if n==words_nums:
+                            result.append(pointer)
+                            word=s[pointer:pointer+word_len]
+                            dict1[word]-=1
+                            pointer+=word_len
+                            n-=1
                 else:
-                    break
-                n+=1
-                if n==words_nums:
-                    result.append(i)
-                    break
+                    for key in dict1:
+                        dict1[key]=0
+                    pointer=pointer+(n+1)*word_len
+                    n = 0
         return result
