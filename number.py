2f0563fac141a858fa25ba0420908d18f968679d
Google1234 <taoJiang_ict@163.com>
2016-09-02 22:37:21 +0800

    ..

--- number.py
+++ number.py
@@ -1,26 +1,40 @@
 class Solution(object):
-    def nextPermutation(self, nums):
+    def findSubstring(self, s, words):
         """
-        :type nums: List[int]
-        :rtype: void Do not return anything, modify nums in-place instead.
+        :type s: str
+        :type words: List[str]
+        :rtype: List[int]
         """
-        length=len(nums)
-        current=length-2
-        while current>=0:
-            if nums[current]<nums[length-1]:
-                t=nums[current]
-                i=current+1
-                while i<length and nums[i]<=t:
-                    i+=1
-                nums[current]=nums[i]
-                nums[i]=t
-                break
+        s_len=len(s)
+        words_nums=len(words)
+        if words_nums==0:
+            return []
+        word_len=len(words[0])
+        if s_len<words_nums*word_len :
+            return []
+        dict={}
+        for word in words:
+            if dict.has_key(word):
+                dict[word]+=1
             else:
-                t=nums[current]
-                i=current+1
-                while i<length and nums[i]<t:
-                    nums[i-1]=nums[i]
-                    i+=1
-                nums[i-1]=t
-                current-=1
-
+                dict[word]=1
+        result=[]
+        for i in xrange(s_len-word_len+1):
+            dict1 = {}
+            n=0
+            while True:
+                word = s[i+n*word_len:i + (n+1)*word_len]
+                if dict.has_key(word):
+                    if dict1.has_key(word):
+                        dict1[word]+=1
+                    else:
+                        dict1[word]=1
+                    if dict1[word]>dict[word]:
+                        break
+                else:
+                    break
+                n+=1
+                if n==words_nums:
+                    result.append(i)
+                    break
+        return result
