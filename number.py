a6b47e7fc223d8dd7ae493a021ad428909661151
Google1234 <taoJiang_ict@163.com>
2016-09-06 22:47:19 +0800

    ..

--- number.py
+++ number.py
@@ -1,25 +1,38 @@
 class Solution(object):
-    def search(self, nums, target):
+    def searchRange(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
-        :rtype: int
+        :rtype: List[int]
         """
+        result=[-1,-1]
         left=0
         right=len(nums)-1
         while left<=right:
             mid=(left+right)/2
             if nums[mid]==target:
-                return mid
+                right1=right
+                mid1=mid
+                right=mid
+                while left<right:
+                    mid=(left+right)/2
+                    if target<=nums[mid]:
+                        right=mid
+                    else:
+                        left=mid+1
+                result[0]=left
+                left=mid1
+                right=right1
+                while left<right:
+                    mid=(left+right+1)/2
+                    if target<nums[mid]:
+                        right=mid-1
+                    else:
+                        left=mid
+                result[1]=right
+                return result
+            elif target<nums[mid]:
+                right=mid-1
             else:
-                if nums[mid]>=nums[left]:
-                    if target>=nums[left] and target<nums[mid]:
-                        right=mid
-                    else:
-                        left=mid+1
-                else:
-                    if target>=nums[left] or target<nums[mid]:
-                        right=mid
-                    else:
-                        left=mid+1
-        return -1
+                left=mid+1
+        return result
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
-a=[1,2,3]
-a.pop()
-print a
+right=1
+right1=right
+right=0
+print right1
