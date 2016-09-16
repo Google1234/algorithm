e3635a904c399468f13151105f0c738baddc3fd9
Google1234 <number1jtao@gmail.com>
2016-09-16 16:14:10 +0800

    ..

--- number.py
+++ number.py
@@ -1,24 +1,30 @@
 class Solution(object):
-    def merge(self, nums1, m, nums2, n):
+    def combinationSum(self, candidates, target):
         """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: void Do not return anything, modify nums1 in-place instead.
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
         """
-        pointer=m+n-1
-        m-=1
-        n-=1
-        while m>=0 and n>=0:
-            if nums1[m]<nums2[n]:
-                nums1[pointer]=nums2[n]
-                n-=1
-            else:
-                nums1[pointer]=nums1[m]
-                m-=1
-            pointer-=1
-        while n>=0:
-            nums1[pointer]=nums2[n]
-            pointer-=1
-            n-=1
+        candidates.sort()
+        result=[]
+        cur=[]
+
+        pointer=0
+        stack=[]
+
+        while pointer<len(candidates):
+                if target>0:
+                    cur.append(candidates[pointer])
+                    target-=candidates[pointer]
+                    stack.append(pointer)
+                else:
+                    if target==0:
+                        result.append([t for t in cur])
+                    if len(cur)<2:
+                        return result
+                    target+=(cur.pop()+cur.pop())
+                    stack.pop()
+                    pointer=stack.pop()+1
+                    while pointer<len(candidates) and candidates[pointer]==candidates[pointer-1]:
+                        pointer+=1
+        return result
