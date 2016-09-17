019579066970560922563c0f6876e4f558f14f57
Google1234 <taojiang_ict@163.com>
2016-09-17 20:37:03 +0800

    ..

--- number.py
+++ number.py
@@ -1,25 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 class Solution(object):
-    def multiply(self, num1, num2):
+    def deleteDuplicates(self, head):
         """
-        :type num1: str
-        :type num2: str
-        :rtype: str
+        :type head: ListNode
+        :rtype: ListNode
         """
-        if num1=='0' or num2=='0':
-            return '0'
-        result=''
-        level=0
-        buff=0
-        while level<(len(num1)+len(num2)-1):
-            for offset in xrange(1,len(num1)+1):
-                offset2=level-offset+2
-                if offset2<=len(num2) and offset2>=1:
-                    buff+=(ord(num1[-offset])-ord('0'))*(ord(num2[-offset2])-ord('0'))
-            result=str(buff%10)+result
-            buff=buff/10
-            level+=1
-        if buff!=0:
-            result=str(buff)+result
-        return result
-
-
+        if head==None:
+            return head
+        node=head
+        while node.next!=None:
+            if node.next.val==node.val:
+                node.next=node.next.next
+            else:
+                node=node.next
+        return head
