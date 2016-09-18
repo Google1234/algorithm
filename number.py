7535fbd8defbdce12cbea1ad565fc9a8489e8064
Google1234 <taojiang_ict@163.com>
2016-09-18 20:43:51 +0800

    ..

--- number.py
+++ number.py
@@ -1,21 +1,40 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
 class Solution(object):
-    def deleteDuplicates(self, head):
+    def isMatch(self, s, p):
         """
-        :type head: ListNode
-        :rtype: ListNode
+        :type s: str
+        :type p: str
+        :rtype: bool
         """
-        if head==None:
-            return head
-        node=head
-        while node.next!=None:
-            if node.next.val==node.val:
-                node.next=node.next.next
+        self.s=s
+        self.p=p
+        self.matrix=[[None for i in xrange(len(self.p))] for j in xrange(len(self.s))]
+        return self.match(0,0)
+    def match(self,s_start,p_start):
+        if s_start<len(self.s) and p_start<len(self.p):
+            if self.matrix[s_start][p_start]==None:
+                if self.p[p_start]=='?':
+                    self.matrix[s_start][p_start]=self.match(s_start+1,p_start+1)
+                elif self.p[p_start]=='*':
+                    i=s_start
+                    while i<=len(self.s) and self.match(i,p_start+1)==False:
+                        i+=1
+                    if i<=len(self.s):
+                        self.matrix[s_start][p_start] = True
+                    else:
+                        self.matrix[s_start][p_start] = False
+                else:
+                    if self.p[p_start]==self.s[s_start]:
+                        self.matrix[s_start][p_start]=self.match(s_start+1,p_start+1)
+                    else:
+                        self.matrix[s_start][p_start]=False
+            return self.matrix[s_start][p_start]
+        else:
+            if s_start==len(self.s):
+                while p_start<len(self.p) and self.p[p_start]=='*':
+                    p_start+=1
+                if p_start==len(self.p):
+                    return True
+                else:
+                    return False
             else:
-                node=node.next
-        return head
+                return False
