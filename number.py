25a8e081772501b62a7cbe791ad63eb20a005f13
Google1234 <taojiang_ict@163.com>
2016-09-18 22:03:52 +0800

    DP

--- number.py
+++ number.py
@@ -5,36 +5,27 @@ class Solution(object):
         :type p: str
         :rtype: bool
         """
-        self.s=s
-        self.p=p
-        self.matrix=[[None for i in xrange(len(self.p))] for j in xrange(len(self.s))]
-        return self.match(0,0)
-    def match(self,s_start,p_start):
-        if s_start<len(self.s) and p_start<len(self.p):
-            if self.matrix[s_start][p_start]==None:
-                if self.p[p_start]=='?':
-                    self.matrix[s_start][p_start]=self.match(s_start+1,p_start+1)
-                elif self.p[p_start]=='*':
-                    i=s_start
-                    while i<=len(self.s) and self.match(i,p_start+1)==False:
-                        i+=1
-                    if i<=len(self.s):
-                        self.matrix[s_start][p_start] = True
-                    else:
-                        self.matrix[s_start][p_start] = False
+        matrix=[[False for i in xrange(len(p)+1)] for j in xrange(len(s)+1)]
+        matrix[len(s)][len(p)]=True
+        j=len(p)-1
+        while j>=0 and p[j]=='*':
+            matrix[len(s)][j]=True
+            j-=1
+        while j>=0:
+            matrix[len(s)][j]=False
+            j-=1
+        i=len(s)-1
+        while i>=0:
+            j = len(p) - 1
+            while j>=0:
+                if p[j]=='?' or p[j]==s[i]:
+                    matrix[i][j]=matrix[i+1][j+1]
+                elif p[j]=='*':
+                    matrix[i][j]=matrix[i][j+1]|matrix[i+1][j]
                 else:
-                    if self.p[p_start]==self.s[s_start]:
-                        self.matrix[s_start][p_start]=self.match(s_start+1,p_start+1)
-                    else:
-                        self.matrix[s_start][p_start]=False
-            return self.matrix[s_start][p_start]
-        else:
-            if s_start==len(self.s):
-                while p_start<len(self.p) and self.p[p_start]=='*':
-                    p_start+=1
-                if p_start==len(self.p):
-                    return True
-                else:
-                    return False
-            else:
-                return False
+                        matrix[i][j]=False
+                j-=1
+            i-=1
+        return matrix[0][0]
+
+
