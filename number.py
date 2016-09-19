786afb0748c623d2b3862b792edd35066f4c5645
Google1234 <taojiang_ict@163.com>
2016-09-19 22:02:53 +0800

    ..

--- number.py
+++ number.py
@@ -1,35 +1,22 @@
 class Solution(object):
-    def isMatch(self, s, p):
+    def jump(self, nums):
         """
-        :type s: str
-        :type p: str
-        :rtype: bool
+        :type nums: List[int]
+        :rtype: int
         """
-        s_pointer=0
-        p_pointer=0
-        last_p_pointer=-1
-        last_s_pointer=0
-        while s_pointer<len(s):
-            if p_pointer<len(p) and (p[p_pointer]=='?' or p[p_pointer]==s[s_pointer]):
-                p_pointer+=1
-                s_pointer+=1
-            elif p_pointer<len(p) and p[p_pointer]=='*':
-                last_p_pointer=p_pointer+1
-                last_s_pointer=s_pointer
-                p_pointer+=1
-            elif last_p_pointer>0:
-                last_s_pointer+=1
-                s_pointer=last_s_pointer
-                p_pointer=last_p_pointer
-            else:
-                return False
-        while p_pointer<len(p) and p[p_pointer]=='*':
-            p_pointer+=1
-        if p_pointer==len(p):
-            return True
-        else:
-            return False
-
-
-
-
+        if len(nums)==1:
+            return 0
+        count=0
+        left=0
+        right=0
+        while True:
+            most_right=right
+            count += 1
+            while left<=right:
+                if left+nums[left]>most_right:
+                    most_right=left+nums[left]
+                    if most_right>=len(nums)-1:
+                        return count
+                left+=1
+            left=right+1
+            right=most_right
