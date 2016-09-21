e68593f425400db177b766360d145761b0877c28
Google1234 <taojiang_ict@163.com>
2016-09-21 20:31:40 +0800

    ..

--- number.py
+++ number.py
@@ -4,6 +4,28 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        self.length=len(nums)
+        self.nums=nums
+        self.result=[]
+        self.DFS(0)
+        return self.result
+    def DFS(self,begin):
+        if begin==self.length:
+            self.result.append([n for n in self.nums])
+        for i in xrange(begin,self.length):
+            buff=self.nums[i]
+            self.nums[i]=self.nums[begin]
+            self.nums[begin]=buff
+            self.DFS(begin+1)
+            buff=self.nums[i]
+            self.nums[i]=self.nums[begin]
+            self.nums[begin]=buff
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
         results=[[]]
         for n in nums:
             new=[]
