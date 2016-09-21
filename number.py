e6e52a516416f44b1159b676119da80bd74cf912
Google1234 <taojiang_ict@163.com>
2016-09-21 20:01:32 +0800

    ..

--- number.py
+++ number.py
@@ -1,35 +1,14 @@
 class Solution(object):
-    def isMatch(self, s, p):
+    def permute(self, nums):
         """
-        :type s: str
-        :type p: str
-        :rtype: bool
+        :type nums: List[int]
+        :rtype: List[List[int]]
         """
-        s_pointer=0
-        p_pointer=0
-        last_p_pointer=-1
-        last_s_pointer=0
-        while s_pointer<len(s):
-            if p_pointer<len(p) and (p[p_pointer]=='?' or p[p_pointer]==s[s_pointer]):
-                p_pointer+=1
-                s_pointer+=1
-            elif p_pointer<len(p) and p[p_pointer]=='*':
-                last_p_pointer=p_pointer+1
-                last_s_pointer=s_pointer
-                p_pointer+=1
-            elif last_p_pointer>0:
-                last_s_pointer+=1
-                s_pointer=last_s_pointer
-                p_pointer=last_p_pointer
-            else:
-                return False
-        while p_pointer<len(p) and p[p_pointer]=='*':
-            p_pointer+=1
-        if p_pointer==len(p):
-            return True
-        else:
-            return False
-
-
-
-
+        results=[[]]
+        for n in nums:
+            new=[]
+            for result in results:
+                for i in xrange(len(result)+1):
+                    new.append(result[:i]+[n]+result[i:])
+            results=new
+        return results
