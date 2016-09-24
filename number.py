158a0a68d66ad25ec073154312c6f651bd89f812
Google1234 <taojiang_ict@163.com>
2016-09-25 00:33:06 +0800

    ..

--- number.py
+++ number.py
@@ -5,34 +5,51 @@ class Solution(object):
         :rtype: int
         """
         self.nums=nums
+        left,right,value=self.divide(0,len(nums)-1)
+        return value
     def divide(self,left,right):
-        mid=(left+right)/2
-        left1,right1,value1=self.divide(left,mid)
-        left2,right2,value2=self.divide(mid,right)
+            mid=(left+right)/2
+            if left==mid:
+                left1, right1, value1 =left,mid,self.nums[left]
+            else:
+                left1,right1,value1=self.divide(left,mid-1)
+            if right==mid:
+                left2, right2, value2=mid,right,self.nums[mid]
+            else:
+                left2,right2,value2=self.divide(mid+1,right)
 
+            right3 = mid
+            sum=self.nums[mid]
+            sum_max=self.nums[mid]
+            pointer = mid + 1
+            while pointer<=right2:
+                sum+=self.nums[pointer]
+                if sum>=sum_max:
+                    sum_max=sum
+                    right3=pointer
+                pointer+=1
+            pointer=mid-1
+            left3=mid
+            sum=sum_max
+            while pointer>=left1:
+                sum+=self.nums[pointer]
+                if sum>=sum_max:
+                    left3=pointer
+                    sum_max=sum
+                pointer-=1
 
-        right3 = mid
-        sum=self.nums[mid]
-        sum_max=self.nums[mid]
-        pointer = mid + 1
-        while pointer<=left2:
-            sum+=self.nums[pointer]
-            if sum>sum_max:
-                sum_max=sum
-                right3=pointer
-            pointer+=1
-        if right3==left2:
-            right3=right2
-            sum_max=sum_max+value2-self.nums[left2]
-        pointer=mid-1
-        left3=mid
-        sum=sum_max
-        while pointer>=right1:
-            sum+=self.nums[pointer]
-            if sum>=sum_max:
-                left3=pointer
-                sum_max=sum_max
-            pointer-=1
-        if left3==right1:
-            sum_max=sum_max+value1-self.nums[right1]
-        if sum_max>
+            if value1>value2:
+                value=value1
+                left=left1
+                right=right1
+            else:
+                value=value2
+                left=left2
+                right=right2
+            if sum_max>value:
+                value=sum_max
+                left=left3
+                right=right3
+            return left,right,value
+s=Solution()
+print s.maxSubArray([1,2,-1,-2,2,1,-2,1,4,-5,4])
