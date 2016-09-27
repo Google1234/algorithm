5840a155743f9b4b43b2b237912dafda6d8425bb
Google1234 <taojiang_ict@163.com>
2016-09-28 01:12:51 +0800

    ..

--- number.py
+++ number.py
@@ -1,20 +1,52 @@
 class Solution(object):
-    def canJump(self, nums):
+    def spiralOrder(self, matrix):
         """
-        :type nums: List[int]
-        :rtype: bool
+        :type matrix: List[List[int]]
+        :rtype: List[int]
         """
-        left=0
-        right=0
-        most_right=right
+        m_most=len(matrix)
+        if m_most==0:
+            return []
+        else:
+            n_most=len(matrix[0])
+        t=0
+        t_most=m_most*n_most
+        layer_nd=0
+        result = [0 for i in xrange(t_most)]
         while True:
-            for i in xrange(left,right+1):
-                if i+nums[i]>=most_right:
-                    most_right=i+nums[i]
-                    if most_right>=len(nums)-1:
-                        return True
-            if most_right==right:
-                return False
-            else:
-                left = right
-                right=most_right
+            m=m_most-layer_nd
+            n=n_most-layer_nd
+            x=layer_nd
+            y=layer_nd
+            if t>=t_most:
+                break
+            while y<n:
+                result[t]=matrix[x][y]
+                y+=1
+                t+=1
+            y-=1
+            x+=1
+            if t>=t_most:
+                break
+            while x<m:
+                result[t]=matrix[x][y]
+                t+=1
+                x+=1
+            x-=1
+            y-=1
+            if t>=t_most:
+                break
+            while y>=layer_nd:
+                result[t]=matrix[x][y]
+                t+=1
+                y-=1
+            y+=1
+            x-=1
+            if t>=t_most:
+                break
+            while x>=layer_nd+1:
+                result[t]=matrix[x][y]
+                t+=1
+                x-=1
+            layer_nd+=1
+        return result
