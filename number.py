e1860acdecbc091cdeec8e0a8d54c4ffa0919b81
Google1234 <taojiang_ict@163.com>
2016-09-28 11:23:10 +0800

    ..

--- number.py
+++ number.py
@@ -1,36 +1,53 @@
 class Solution(object):
-    def permute(self, nums):
+    def merge(self, intervals):
         """
-        :type nums: List[int]
-        :rtype: List[List[int]]
+        :type nums: List[Interval]
+        :rtype: List[Interval]
         """
-        self.length=len(nums)
-        self.nums=nums
-        self.result=[]
-        self.DFS(0)
-        return self.result
-    def DFS(self,begin):
-        if begin==self.length:
-            self.result.append([n for n in self.nums])
-        for i in xrange(begin,self.length):
-            buff=self.nums[i]
-            self.nums[i]=self.nums[begin]
-            self.nums[begin]=buff
-            self.DFS(begin+1)
-            buff=self.nums[i]
-            self.nums[i]=self.nums[begin]
-            self.nums[begin]=buff
-class Solution(object):
-    def permute(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        results=[[]]
-        for n in nums:
-            new=[]
-            for result in results:
-                for i in xrange(len(result)+1):
-                    new.append(result[:i]+[n]+result[i:])
-            results=new
-        return results
+        if intervals==[]:
+            return []
+        self.intervals=intervals
+        self.sort(0,len(intervals)-1)
+        result=[]
+        left=0
+        right=1
+        while right<len(self.intervals):
+            if self.intervals[left].end<self.intervals[right].start:
+                result.append(self.intervals[left])
+                left=right
+            else:
+                if self.intervals[left].end<self.intervals[right].end:
+                    self.intervals[left].end=self.intervals[right].end
+            right+=1
+        result.append(self.intervals[left])
+        return result
+
+    def sort(self,left,right):
+        if left<right:
+            mid=(left+right)/2
+            self.sort(left,mid)
+            self.sort(mid+1,right)
+            buff=[Interval() for i in xrange(right-left+1)]
+            pointer1=left
+            pointer2=mid+1
+            pointer=0
+            while pointer1<=mid and pointer2<=right:
+                if self.intervals[pointer1].start<self.intervals[pointer2].start:
+                    buff[pointer]=self.intervals[pointer1]
+                    pointer1+=1
+                else:
+                    buff[pointer]=self.intervals[pointer2]
+                    pointer2+=1
+                pointer+=1
+            while pointer1<=mid:
+                buff[pointer]=self.intervals[pointer1]
+                pointer1+=1
+                pointer+=1
+            while pointer2<=right:
+                buff[pointer]=self.intervals[pointer2]
+                pointer2+=1
+                pointer+=1
+            i=0
+            while i<=right-left:
+                self.intervals[left+i]=buff[i]
+                i+=1
--- test.py
+++ test.py
@@ -1,2 +1,4 @@
-r="12"
-print r[:1]
+for i in xrange(10):
+    if i==5:
+        continue
+    print i
