6a5abea45be5e5a983ecb5683cc737dcc9fe4b8f
Google1234 <taojiang_ict@163.com>
2016-10-05 20:51:45 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	number.py

--- number.py
+++ number.py
@@ -1,26 +1,31 @@
 class Solution(object):
-    def uniquePaths(self, m, n):
+    def uniquePathsWithObstacles(self, obstacleGrid):
         """
-        :type m: int
-        :type n: int
+        :type obstacleGrid: List[List[int]]
         :rtype: int
         """
-        self.matrix=[[0 for i in xrange(n)] for j in xrange(m)]
-        for i in xrange(m):
-            self.matrix[i][0]=1
-        for i in xrange(n):
-            self.matrix[0][i]=1
-        return self.interation(m,n)
-    def interation(self,m,n):
-        if m==0 or n==0:
+        self.m=len(obstacleGrid)
+        self.n=len(obstacleGrid[0])
+        self.matrix=[[-1 for i in xrange(self.n)] for j in xrange(self.m)]
+        self.matrix[self.m-1][self.n-1]=1
+        self.obstacleGrid=obstacleGrid
+        if obstacleGrid[0][0]==1:
             return 0
-        if self.matrix[m-1][n-1]!=0:
-            return self.matrix[m-1][n-1]
-        if m==1 or n==1:
-            self.matrix[m-1][n-1]=1
-        else:
-            self.matrix[m-1][n-1]=self.interation(m-1,n)+self.interation(m,n-1)
-        return self.matrix[m-1][n-1]
+        return self.interation(0,0)
 
+    def interation(self,m,n):
+        if self.matrix[m][n]!=-1:
+            return self.matrix[m][n]
+        if m+1<self.m and self.obstacleGrid[m+1][n]==0:
+            if n+1<self.n and self.obstacleGrid[m][n+1]==0:
+                self.matrix[m][n]=self.interation(m+1,n)+self.interation(m,n+1)
+            else:
+                self.matrix[m][n] = self.interation(m + 1, n)
+        else:
+            if n+1<self.n and self.obstacleGrid[m][n+1]==0:
+                self.matrix[m][n]=self.interation(m,n+1)
+            else:
+                self.matrix[m][n] = 0
+        return self.matrix[m][n]
 s=Solution()
 print s.uniquePaths(3,7)
