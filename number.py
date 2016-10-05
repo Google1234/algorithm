3aeb50f2d0d83dbcd3feb547c56f3844ee626da7
Google1234 <taojiang_ict@163.com>
2016-10-05 20:01:03 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	number.py

--- number.py
+++ number.py
@@ -1,36 +1,30 @@
-class Node(object):
-    def __init__(self,val,next=None):
-        self.val=val
-        self.next=next
+# Definition for singly-linked list.
+class ListNode(object):
+     def __init__(self, x):
+         self.val = x
+         self.next = None
 class Solution(object):
-    def getPermutation(self, n, k):
+    def rotateRight(self, head, k):
         """
-        :type n: int
+        :type head: ListNode
         :type k: int
-        :rtype: str
+        :rtype: ListNode
         """
-        result=""
-        start=Node(-1,None)
-        b=start
-        i=1
-        mul=1
-        while i<=n:
-            a=Node(i,None)
-            b.next=a
-            b=a
-            mul=mul*i
-            i+=1
-
-
-        while n!=0:
-            mul=mul/n
-            pointer=start
-            base_k=0
-            while base_k+mul<k:
-                pointer=pointer.next
-                base_k+=mul
-            result+=str(pointer.next.val)
-            pointer.next=pointer.next.next
-            k-=base_k
-            n-=1
+        if head==None:
+            return None
+        len=1
+        pointer=head
+        while pointer.next!=None:
+            pointer=pointer.next
+            len+=1
+        pointer.next=head
+        k=k%len
+        k=len-k
+        pointer=head
+        t=1
+        while t<k:
+            t+=1
+            pointer=pointer.next
+        result=pointer.next
+        pointer.next=None
         return result
