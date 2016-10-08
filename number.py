77fec6e2c3a61e0883ecb17b3074a53231ba2f6f
Google1234 <taojiang_ict@163.com>
2016-10-08 21:29:47 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	number.py

--- number.py
+++ number.py
@@ -1,45 +1,32 @@
 class Solution(object):
-    def solveSudoku(self, board):
+    def combinationSum(self, candidates, target):
         """
-        :type board: List[List[str]]
-        :rtype: void Do not return anything, modify board in-place instead.
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
         """
-        if len(board)==9 and len(board[0])==9:
-            self.board=[list(board[i]) for i in xrange(9)]
-            self.backtrack(0)
-            for i in xrange(9):
-                board[i]=''.join(self.board[i])
+        if candidates==[]:
+            return []
+        self.result=[]
+        self.stop=len(candidates)
+        self.candiates=candidates
+        self.candiates.sort()
+        self.stack=[]
+        self.backtract(0,target)
+        return self.result
 
-    def backtrack(self,i):
-        if i>=81:
-            return True
+    def backtract(self,start,target):
+        if target==0:
+            self.result.append([i for i in self.stack])
         else:
-            x=i/9
-            y=i%9
-            if self.board[x][y]!='.':
-                if self.valid(x,y)==True:
-                    return self.backtrack(i+1)
+            for pointer in range(start,self.stop):
+                if self.candiates[pointer]>target:
+                    break
                 else:
-                    return False
-            else:
-                for num in "123456789":
-                    self.board[x][y]=num
-                    if self.valid(x,y)==True and self.backtrack(i+1)==True:
-                        return True
-                    self.board[x][y]='.'
-                return False
-
-    def valid(self,x,y):
-        for i in xrange(9):
-                if i!=y and self.board[x][i]==self.board[x][y]:
-                    return False
-        for i in xrange(9):
-                if i!=x and self.board[i][y] == self.board[x][y]:
-                    return False
-        for i in xrange((x/3)*3,(x/3+1)*3):
-            for j in xrange((y/3)*3,(y/3+1)*3):
-                if (i!=x and j!=y) and self.board[i][j]==self.board[x][y]:
-                    return False
-        return True
+                    if pointer==start or self.candiates[pointer]!=self.candiates[pointer-1]:
+                        self.stack.append(self.candiates[pointer])
+                        self.backtract(pointer,target-self.candiates[pointer])
+                        self.stack.pop()
 s=Solution()
-s.solveSudoku(["..9748...","7........",".2.1.9...","..7...24.",".64.1.59.",".98...3..","...8.3.2.","........6","...2759.."])
+print s.combinationSum([2,3,6,7]
+,7)
