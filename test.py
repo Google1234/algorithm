806f9649c4a142f1ab6f5ec25823404fdf765b4b
Google1234 <number1jtao@gmail.com>
2016-11-06 22:51:09 +0800

    00

--- test.py
+++ test.py
@@ -1,21 +1,17 @@
 class Solution(object):
-    def isPalindrome(self, s):
+    def singleNumber(self, nums):
         """
-        :type s: str
-        :rtype: bool
+        :type nums: List[int]
+        :rtype: int
         """
-        left=0
-        right=len(s)-1
-        while left<=right:
-            if (48<=ord(s[left])<=57 or 65<=ord(s[left])<=90 or 97<=ord(s[left])<=122)==False:
-                left+=1
-                continue
-            if (48<=ord(s[right])<=57 or 65<=ord(s[right])<=90 or 97<=ord(s[right])<=122)==False:
-                right-=1
-                continue
-            if (s[left]==s[right]) or (ord(s[left])>48 and ord(s[right])>48 and (ord(s[left])-ord(s[right])==32 or ord(s[left])-ord(s[right])==-32)):
-                left += 1
-                right -= 1
+        dic={}
+        for num in nums:
+            if num in dic.keys():
+                dic[num]+=1
             else:
-                return  False
-        return True
+                dic[num]=1
+        for num in nums:
+            if dic[num]==1:
+                return num
+a=Solution()
+print a.singleNumber([1])
