aee6be02d36771cbba32662b92eb33dbf3985ace
Number1Tao <taoJiang_ict@163.com>
2016-12-03 09:42:26 -0500

    ..

--- number.py
+++ number.py
@@ -1,51 +1,10 @@
-# Definition for a binary tree node.
-
 class Solution(object):
-    def levelOrderBottom(self, root):
+    def titleToNumber(self, s):
         """
-        :type root: TreeNode
-        :rtype: List[List[int]]
+        :type s: str
+        :rtype: int
         """
-        if root==None:
-            return []
-        else:
-            buff=[[root],[]]
-            cur_use=0
-            cur_not_use=1-cur_use
-            cur_position=[0,0]
-            buff_len=[1,0]
-            buff_use_len=[1,0]
-            result=[[]]
-            deepth=0
-        while True:
-            node=buff[cur_use][cur_position[cur_use]]
-            result[deepth].append(node.val)
-            if node.left!=None:
-                if cur_position[cur_not_use]==buff_len[cur_not_use]:
-                    buff[cur_not_use].append(node.left)
-                    buff_len[cur_not_use] += 1
-                else:
-                    buff[cur_not_use][cur_position[cur_not_use]]=node.left
-                cur_position[cur_not_use]+=1
-                buff_use_len[cur_not_use]+=1
-            if node.right!=None:
-                if cur_position[cur_not_use]==buff_len[cur_not_use]:
-                    buff[cur_not_use].append(node.right)
-                    buff_len[cur_not_use]+=1
-                else:
-                    buff[cur_not_use][cur_position[cur_not_use]] = node.right
-                cur_position[cur_not_use] += 1
-                buff_use_len[cur_not_use] += 1
-            cur_position[cur_use]+=1
-            if cur_position[cur_use]==buff_use_len[cur_use]:
-                if buff_use_len[cur_not_use]==0:
-                    break
-                cur_position[cur_use]=0
-                cur_position[1-cur_use]=0
-                buff_use_len[cur_use]=0
-                cur_not_use=cur_use
-                cur_use=1-cur_use
-                result.append([])
-                deepth+=1
-        result.reverse()
+        result=0
+        for c in s:
+            result=result*26+(ord(c)-ord('A')+1)
         return result
