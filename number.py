f67343690733495d1f87696def642858a55612d3
Number1Tao <taojiang_ict@163.com>
2017-02-28 10:12:53 +0800

    Word Ladder II

--- number.py
+++ number.py
@@ -1,10 +1,60 @@
 class Solution(object):
-    def titleToNumber(self, s):
+    def findLadders(self, beginWord, endWord, wordList):
         """
-        :type s: str
-        :rtype: int
+        :type beginWord: str
+        :type endWord: str
+        :type wordList: List[str]
+        :rtype: List[List[str]]
         """
-        result=0
-        for c in s:
-            result=result*26+(ord(c)-ord('A')+1)
-        return result
+        words=set(wordList)
+        if endWord not in words:
+            return []
+        prev_dic={}
+        for word in words:
+            prev_dic[word]=[]
+        prev_dic[endWord]=[]
+        befores=set()
+        befores.add(beginWord)
+        ends=set()
+        ends.add(endWord)
+        import sys
+        _min=sys.maxint
+        dis=1
+        forward=True
+        while len(befores)>0:
+            words-=befores
+            candinates=set()
+            dis+=1
+            if dis>_min:
+                break
+            for word in befores:
+                candinate=words&set([word[:i]+c+word[i+1:] for c in 'abcdefghijklmnopqrstuvwxyz' for i in range(len(beginWord))])
+                for cc in candinate:
+                    if forward:
+                    	prev_dic[cc].append(word)
+                    else:
+                        prev_dic[word].append(cc)
+                candinates|=candinate
+            if candinates&ends:
+                _min=dis
+            befores=candinates
+            if len(befores)>len(ends):
+                ends,befores=befores,ends
+                forward=not forward
+        if _min==sys.maxint:
+            return []
+        else:
+            self.rst=[]
+            self.endword=endWord
+            self.buff=['' for i in range(_min)]
+            self.prev=prev_dic
+            index=_min-1
+            self.dfs(index,endWord)
+            return self.rst
+    def dfs(self,index,word):
+        self.buff[index]=word
+        if index==0:
+            self.rst.append([cc for cc in self.buff])
+        else:
+            for cc in self.prev[word]:
+                self.dfs(index-1,cc)
