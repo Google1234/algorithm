865961f6bb7304e51ccdc46c0b0ccad6e64c8c19
Number1Tao <taojiang_ict@163.com>
2017-02-28 11:09:27 +0800

    Word Ladder II

--- number.py
+++ number.py
@@ -1,60 +1,29 @@
 class Solution(object):
-    def findLadders(self, beginWord, endWord, wordList):
+    def evalRPN(self, tokens):
         """
-        :type beginWord: str
-        :type endWord: str
-        :type wordList: List[str]
-        :rtype: List[List[str]]
+        :type tokens: List[str]
+        :rtype: int
         """
-        words=set(wordList)
-        if endWord not in words:
-            return []
-        prev_dic={}
-        for word in words:
-            prev_dic[word]=[]
-        prev_dic[endWord]=[]
-        befores=set()
-        befores.add(beginWord)
-        ends=set()
-        ends.add(endWord)
-        import sys
-        _min=sys.maxint
-        dis=1
-        forward=True
-        while len(befores)>0:
-            words-=befores
-            candinates=set()
-            dis+=1
-            if dis>_min:
-                break
-            for word in befores:
-                candinate=words&set([word[:i]+c+word[i+1:] for c in 'abcdefghijklmnopqrstuvwxyz' for i in range(len(beginWord))])
-                for cc in candinate:
-                    if forward:
-                    	prev_dic[cc].append(word)
-                    else:
-                        prev_dic[word].append(cc)
-                candinates|=candinate
-            if candinates&ends:
-                _min=dis
-            befores=candinates
-            if len(befores)>len(ends):
-                ends,befores=befores,ends
-                forward=not forward
-        if _min==sys.maxint:
-            return []
-        else:
-            self.rst=[]
-            self.endword=endWord
-            self.buff=['' for i in range(_min)]
-            self.prev=prev_dic
-            index=_min-1
-            self.dfs(index,endWord)
-            return self.rst
-    def dfs(self,index,word):
-        self.buff[index]=word
-        if index==0:
-            self.rst.append([cc for cc in self.buff])
-        else:
-            for cc in self.prev[word]:
-                self.dfs(index-1,cc)
+        pointer1=0
+        pointer2=1
+        while pointer2<len(tokens):
+            if tokens[pointer2]=='+':
+                tokens[pointer1-1]=int(tokens[pointer1-1])+int(tokens[pointer1])
+                pointer1-=1
+            elif tokens[pointer2]=='-':
+                tokens[pointer1-1]=int(tokens[pointer1-1])-int(tokens[pointer1])
+                pointer1-=1
+            elif tokens[pointer2]=='*':
+                tokens[pointer1-1]=int(tokens[pointer1-1])*int(tokens[pointer1])
+                pointer1-=1
+            elif tokens[pointer2]=='/':
+                tokens[pointer1-1]=int(tokens[pointer1-1])*1.0/int(tokens[pointer1])
+                pointer1-=1
+            else:
+                pointer1+=1
+                tokens[pointer1]=tokens[pointer2]
+            pointer2+=1
+        return int(tokens[0])
+
+s=Solution()
+print s.evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"])
--- test.py
+++ test.py
@@ -1,17 +1,63 @@
 class Solution(object):
-    def singleNumber(self, nums):
+    def findLadders(self, beginWord, endWord, wordList):
         """
-        :type nums: List[int]
-        :rtype: int
+        :type beginWord: str
+        :type endWord: str
+        :type wordList: List[str]
+        :rtype: List[List[str]]
         """
-        dic={}
-        for num in nums:
-            if num in dic.keys():
-                dic[num]+=1
-            else:
-                dic[num]=1
-        for num in nums:
-            if dic[num]==1:
-                return num
-a=Solution()
-print a.singleNumber([1])
+        words=set(wordList)
+        prev_dic={}
+        for word in words:
+            prev_dic[word]=[]
+        prev_dic[endWord]=[]
+        befores=set()
+        befores.add(beginWord)
+        ends=set()
+        ends.add(endWord)
+        import sys
+        _min=sys.maxint
+        dis=1
+        forward=True
+        while len(befores)>0:
+            words-=befores
+            candinates=set()
+            dis+=1
+            if dis>_min:
+                break
+            for word in befores:
+                candinate=words&set([word[:i]+c+word[i+1:] for c in 'abcdefghijklmnopqrstuvwxyz' for i in range(len(beginWord))])
+                for cc in candinate:
+                    if forward:
+                    	prev_dic[cc].append(word)
+                    else:
+                        prev_dic[word].append(cc)
+                candinates|=candinate
+            if candinates&ends:
+                _min=dis
+            befores=candinates
+            if len(befores)>len(ends):
+                ends,befores=befores,ends
+                forward=not forward
+        if _min==sys.maxint:
+            return []
+        else:
+            self.rst=[]
+            self.endword=endWord
+            self.buff=['' for i in range(_min)]
+            self.prev=prev_dic
+            index=_min-1
+            self.dfs(index,endWord)
+            return self.rst
+    def dfs(self,index,word):
+        self.buff[index]=word
+        if index==0:
+            self.rst.append([cc for cc in self.buff])
+        else:
+            for cc in self.prev[word]:
+                self.dfs(index-1,cc)
+
+s=Solution()
+print s.findLadders("hit",
+"cog",
+["hot","dot","dog","lot","log","cog"])
