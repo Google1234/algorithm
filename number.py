1b610d5c756f3071f8810bb72501a45280b447af
Google1234 <taojiang_ict@163.com>
2017-03-07 09:36:02 +0800

    Maximum Product Subarray

--- number.py
+++ number.py
@@ -1,25 +1,22 @@
 class Solution(object):
-    def maxProduct(self, nums):
+    def singleNumber(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        p_max=[]
-        n_min=[]
-        p_max.append(1)
-        n_min.append(1)
-        global_max=nums[0]
+        one=0
+        two=0
+        three=0
+        four=0
         for num in nums:
-            if num>0:
-                p_max.append(max(num,p_max[-1]*num))
-                n_min.append(min(num,n_min[-2]*num))
-            else:
-                p_max.append(max(num,n_min[-1]*num))
-                n_min.append(min(num,p_max[-2]*num))
-            if p_max[-1]>global_max:
-                global_max=p_max[-1]
-        return global_max
-
+            three|=(two&one)
+            two|=(one&num)
+            one^=num
+            four=three&(~one)
+            one&=(~four)
+            two&=(~four)
+            three&=(~four)
+        return one
 
 s=Solution()
-print s.maxProduct([-4,-3,-2])
+print s.singleNumber([2,3,4,1,2,3,4,2,3,4,4,3,2,1,1,1,5])
